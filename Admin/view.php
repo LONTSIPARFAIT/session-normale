383b172927da018384403639b634c4e28dd3a3bf
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-03 17:50:10 +0100

    first commit

--- Admin/view.php
+++ Admin/view.php
@@ -1,3 +1,6 @@
 <?php 
 // Inclusion du fichier de configuration de la base de données
 require_once '../db.php';
+
+$message = ''; // Variable pour stocker les messages d'état
+$id = intval($_GET['id']); // Récupérer l'ID du groupe à éditer, en s'assurant qu'il s'agit d'un entier
