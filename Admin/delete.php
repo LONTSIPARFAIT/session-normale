0b1e76e6713ca12fe37bb74dda0c8f1003b285eb
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-03 17:36:45 +0100

    first commit

--- Admin/delete.php
+++ Admin/delete.php
@@ -6,3 +6,6 @@ require_once '../db.php';
 $message = '';
 
 // Vérification si un identifiant de groupe a été passé dans l'URL
+if (isset($_GET['id'])) {
+    // Conversion de l'ID en entier pour éviter les injections SQL
+}
