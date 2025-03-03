222815024c6e375364d1e47f3f6ba2312433bbe3
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-03 17:50:27 +0100

    first commit

--- Admin/view.php
+++ Admin/view.php
@@ -4,3 +4,9 @@ require_once '../db.php';
 
 $message = ''; // Variable pour stocker les messages d'état
 $id = intval($_GET['id']); // Récupérer l'ID du groupe à éditer, en s'assurant qu'il s'agit d'un entier
+
+// Vérifier si le groupe existe dans la base de données
+$sql = "SELECT * FROM `groupe` WHERE `id` = ?";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$id]);
+$groupe = $stmt->fetch(); // Récupérer les détails du groupe
