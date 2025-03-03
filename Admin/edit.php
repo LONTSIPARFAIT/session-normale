a4a7ddde2a39154b7c05154576d6ba2e1dc1ded6
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-03 17:46:37 +0100

    first commit

--- Admin/edit.php
+++ Admin/edit.php
@@ -1,2 +1,11 @@
 <?php 
 require_once '../db.php';
+
+$message = '';
+$id = intval($_GET['id']); // Récupérer l'ID du groupe à éditer
+
+// Vérifier si le groupe existe
+$sql = "SELECT * FROM `groupe` WHERE `id` = ?";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$id]);
+$groupe = $stmt->fetch();
